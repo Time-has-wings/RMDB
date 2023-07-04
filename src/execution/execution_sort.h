1ae0f847b2b481203087e09d2f226b1d20fa1d68
pygone <pygonebe@outlook.com>
2023-07-04 19:34:22 +0800

    one col sort

--- src/execution/execution_sort.h
+++ src/execution/execution_sort.h
@@ -14,36 +14,111 @@ See the Mulan PSL v2 for more details. */
 #include "executor_abstract.h"
 #include "index/ix.h"
 #include "system/sm.h"
+#include <queue>
+struct value_id
+{
+    Value v;
+    size_t id;
+    bool operator<(const value_id &t)
+    {
+        return v < t.v;
+    }
+    bool operator>(const value_id &t)
+    {
+        return v > t.v;
+    }
+};
 
-class SortExecutor : public AbstractExecutor {
-   private:
+class SortExecutor : public AbstractExecutor
+{
+private:
     std::unique_ptr<AbstractExecutor> prev_;
-    ColMeta cols_;                              // 框架中只支持一个键排序，需要自行修改数据结构支持多个键排序
+    ColMeta cols_; // 框架中只支持一个键排序，需要自行修改数据结构支持多个键排序
     size_t tuple_num;
     bool is_desc_;
     std::vector<size_t> used_tuple;
+    std::queue<size_t> sorted_tuples;
     std::unique_ptr<RmRecord> current_tuple;
 
-   public:
-    SortExecutor(std::unique_ptr<AbstractExecutor> prev, TabCol sel_cols, bool is_desc) {
+public:
+    SortExecutor(std::unique_ptr<AbstractExecutor> prev, TabCol sel_cols, bool is_desc)
+    {
         prev_ = std::move(prev);
-        cols_ = prev_->get_col_offset(sel_cols);
+        auto &prev_cols = prev_->cols();
+        auto pos = get_col(prev_cols, sel_cols);
+        cols_ = *pos;
         is_desc_ = is_desc;
         tuple_num = 0;
         used_tuple.clear();
+        sort();
+    }
+    const std::vector<ColMeta> &cols() const {
+        return prev_->cols();
+    };
+    bool is_end() const override
+    {
+        return used_tuple.size() > tuple_num;
+    }
+    void beginTuple() override
+    {
+        search();
     }
 
-    void beginTuple() override { 
-        
+    void nextTuple() override
+    {
+        search();
     }
 
-    void nextTuple() override {
-        
+    std::unique_ptr<RmRecord> Next() override
+    {
+        auto rec = current_tuple.get();
+        auto ret = std::make_unique<RmRecord>(cols_.len);
+        memcpy(ret->data, rec->data, rec->size);
+        return ret;
     }
+    void sort()
+    {
 
-    std::unique_ptr<RmRecord> Next() override {
-        return nullptr;
+        std::vector<value_id> v;
+        size_t id = 0;
+        prev_->beginTuple();
+        while (!prev_->is_end())
+        {
+            tuple_num ++;
+            auto rec = prev_->Next();
+            auto val = prev_->get_value(rec, cols_);
+            v.push_back({val, id++});
+            prev_->nextTuple();
+        }
+        if (!is_desc_)
+            std::sort(v.begin(), v.end());
+        else
+            std::sort(v.rbegin(), v.rend());
+        for (auto i : v)
+        {
+            sorted_tuples.push(i.id);
+        }
     }
-
+    void search()
+    {
+        prev_->beginTuple();
+        size_t id = 0;
+        auto x = sorted_tuples.front();
+        sorted_tuples.pop();
+        used_tuple.push_back(x);
+        while (!prev_->is_end())
+        {
+            if (id++ != x)
+            {
+                prev_->nextTuple();
+                continue;
+            }
+            else
+            {
+                current_tuple = prev_->Next();
+                break;
+            }
+        }
+    }
     Rid &rid() override { return _abstract_rid; }
 };
