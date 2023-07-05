0b77a8191ddfc4189f02a0263f9a7706ce1de4f3
pygone <pygonebe@outlook.com>
2023-07-05 19:55:05 +0800

    use block join

--- src/execution/execution_manager.cpp
+++ src/execution/execution_manager.cpp
@@ -13,7 +13,7 @@ See the Mulan PSL v2 for more details. */
 #include "executor_delete.h"
 #include "executor_index_scan.h"
 #include "executor_insert.h"
-#include "executor_nestedloop_join.h"
+#include "executor_block_nestedloop_join.h"
 #include "executor_projection.h"
 #include "executor_seq_scan.h"
 #include "executor_update.h"
