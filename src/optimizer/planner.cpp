8d080426b35bd014bb69d6785b89b384f6db1aa1
pygone <pygonebe@outlook.com>
2023-07-10 14:08:13 +0800

    test back to block join

--- src/optimizer/planner.cpp
+++ src/optimizer/planner.cpp
@@ -15,7 +15,7 @@ See the Mulan PSL v2 for more details. */
 #include "execution/executor_delete.h"
 #include "execution/executor_index_scan.h"
 #include "execution/executor_insert.h"
-#include "execution/executor_nestedloop_join.h"
+#include "execution/executor_block_nestedloop_join.h"
 #include "execution/executor_projection.h"
 #include "execution/executor_seq_scan.h"
 #include "execution/executor_update.h"
