e4471f35d4935e6f85ad4db3ac1b00ae8235c2db
pygone <pygonebe@outlook.com>
2023-07-23 18:16:56 +0800

    test

--- src/execution/executor_update.h
+++ src/execution/executor_update.h
@@ -25,6 +25,9 @@ private:
     std::string tab_name_;
     std::vector<SetClause> set_clauses_;
     SmManager *sm_manager_;
+    std::vector<IndexMeta > index_queue;
+    std::vector<ColMeta > col_queue;
+    std::vector<RmRecord > rm_vector;
 
 public:
     UpdateExecutor(SmManager *sm_manager, const std::string &tab_name, std::vector<SetClause> set_clauses,
@@ -41,9 +44,19 @@ public:
     }
     std::unique_ptr<RmRecord> Next() override
     {
+        if (context_->txn_->get_txn_mode())
+        {
+            for (auto &rid : rids_)
+            {
+                bool res = (context_->lock_mgr_->lock_exclusive_on_record(context_->txn_, rid, fh_->GetFd()));
+                if (res == false)
+                    throw TransactionAbortException(context_->txn_->get_transaction_id(), AbortReason::DEADLOCK_PREVENTION);
+            }
+        }
         for (auto &clause : set_clauses_)
         {
             auto col = tab_.get_col(clause.lhs.col_name);
+            col_queue.push_back(*col);
             clause.rhs.value_change(col->type);
             if (clause.rhs.type != col->type)
             {
@@ -66,90 +79,68 @@ public:
                 break;
             }
         }
-        if (context_->txn_->get_txn_mode())
+        for (auto &rid : rids_)
         {
-            for (auto &rid : rids_)
+            auto rec = *(fh_->get_record(rid, context_));
+            for (auto &clause : set_clauses_)
             {
-                bool res = (context_->lock_mgr_->lock_exclusive_on_record(context_->txn_, rid, fh_->GetFd()));
-                if (res == false)
-                    throw TransactionAbortException(context_->txn_->get_transaction_id(), AbortReason::DEADLOCK_PREVENTION);
+                auto col = tab_.get_col(clause.lhs.col_name);
+                memcpy(rec.data + col->offset, clause.rhs.raw->data, col->len);
             }
+            rm_vector.push_back(rec);
         }
         for (auto &index : tab_.indexes)
         {
+            for (auto &col : col_queue)
+            {
+                if (std::any_of(index.cols.begin(), index.cols.end(), [&col](const ColMeta &c)
+                                { return col.name == c.name; }))
+                {
+                    index_queue.push_back(index);
+                    break;
+                }
+            }
+        }
+        for (auto &index : index_queue)
+        {
             char key_old[index.col_tot_len];
             char key[index.col_tot_len];
-            for (auto &rid : rids_)
+            for (size_t i = 0; i < rids_.size(); i++)
             {
-                std::unique_ptr<RmRecord> rec = fh_->get_record(rid, context_);
-                char dat[rec->size];
-                memcpy(dat, rec->data, rec->size);
-                for (auto &clause : set_clauses_)
+                auto &rec_i = rm_vector[i];
+                int offset_i = 0;
+                for (size_t k = 0; k < index.col_num; ++k)
                 {
-                    auto col = tab_.get_col(clause.lhs.col_name);
-                    memcpy(dat + col->offset, clause.rhs.raw->data, col->len);
+                    memcpy(key_old + offset_i, rec_i.data + index.cols[k].offset, index.cols[k].len);
+                    offset_i += index.cols[k].len;
                 }
-                int offset = 0;
-                for (size_t i = 0; i < index.col_num; ++i)
+                for (size_t j = i + 1; j < rids_.size(); j++)
                 {
-                    memcpy(key + offset, dat + index.cols[i].offset, index.cols[i].len);
-                    offset += index.cols[i].len;
-                }
-                std::memcpy(key_old, key, index.col_tot_len);
-                int cnt = 0;
-                for (auto &rid : rids_)
-                {
-                    std::unique_ptr<RmRecord> rec = fh_->get_record(rid, context_);
-                    char dat[rec->size];
-                    memcpy(dat, rec->data, rec->size);
-                    for (auto &clause : set_clauses_)
+                    auto &rec_j = rm_vector[j];
+                    int offset_j = 0;
+                    for (size_t k = 0; k < index.col_num; ++k)
                     {
-                        auto col = tab_.get_col(clause.lhs.col_name);
-                        memcpy(dat + col->offset, clause.rhs.raw->data, col->len);
-                    }
-                    int offset = 0;
-                    for (size_t i = 0; i < index.col_num; ++i)
-                    {
-                        memcpy(key + offset, dat + index.cols[i].offset, index.cols[i].len);
-                        offset += index.cols[i].len;
+                        memcpy(key + offset_j, rec_j.data + index.cols[k].offset, index.cols[k].len);
+                        offset_j += index.cols[k].len;
                     }
                     if (memcmp(key_old, key, index.col_tot_len) == 0)
-                        cnt++;
-                    if (cnt >= 2)
                         throw IndexEnrtyExistsError();
                 }
             }
         }
-        for (auto &rid : rids_)
+        for (size_t i = 0; i < rids_.size(); i++)
         {
+            auto update_record = rm_vector[i];
+            auto &rid = rids_[i];
             std::unique_ptr<RmRecord> rec = fh_->get_record(rid, context_);
-            RmRecord update_record(rec->size);
-            memcpy(update_record.data, rec->data, rec->size);
-            for (auto &clause : set_clauses_)
-            {
-                auto col = tab_.get_col(clause.lhs.col_name);
-                memcpy(update_record.data + col->offset, clause.rhs.raw->data, col->len);
-            }
             UpdateLogRecord update_log(context_->txn_->get_transaction_id(), *(rec.get()), update_record, rid, tab_name_);
             update_log.prev_lsn_ = context_->txn_->get_prev_lsn();
             context_->log_mgr_->add_log_to_buffer(&update_log);
             context_->txn_->set_prev_lsn(update_log.lsn_);
-        }
-        // context_->log_mgr_->flush_log_to_disk();
-        for (auto &rid : rids_)
-        {
-            std::unique_ptr<RmRecord> rec = fh_->get_record(rid, context_);
-            RmRecord update_record(rec->size);
-            memcpy(update_record.data, rec->data, rec->size);
-            for (auto &clause : set_clauses_)
-            {
-                auto col = tab_.get_col(clause.lhs.col_name);
-                memcpy(update_record.data + col->offset, clause.rhs.raw->data, col->len);
-            }
-            for (auto &index : tab_.indexes)
+            for (auto &index : index_queue)
             {
                 auto ihs = sm_manager_->ihs_.at(sm_manager_->get_ix_manager()->get_index_name(tab_.name, index.cols)).get();
-                char update [index.col_tot_len];
+                char update[index.col_tot_len];
                 char orign[index.col_tot_len];
                 int offset = 0;
                 for (size_t i = 0; i < index.col_num; ++i)
