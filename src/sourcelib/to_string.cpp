6859c9b8b0a9ac13cd51146d6bb298a630cb2372
pygone <pygonebe@outlook.com>
2023-08-03 10:23:36 +0800

    to_string

---
+++ src/sourcelib/to_string.cpp
@@ -0,0 +1,22 @@
+#include "to_string.h"
+#include "fmt/format.h"
+std::string To_string(const char* src, const ColMeta& col)
+{
+	std::string s;
+	switch (col.type)
+	{
+	case TYPE_INT:
+		return fmt::to_string<int>(*(int*)src);
+	case TYPE_FLOAT:
+		fmt::format_to(std::back_inserter(s), "{:.6f}", *(float*)src);
+		return s;
+	case TYPE_BIGINT:
+		return fmt::to_string<int64_t>(*(int64_t*)src);
+	case TYPE_DATETIME:
+		return trans_datetime(*(int64_t*)src);
+	case TYPE_STRING:
+		return { src, static_cast<size_t>(col.len) };
+	default:
+		break;
+	}
+}
